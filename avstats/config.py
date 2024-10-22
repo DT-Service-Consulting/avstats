7f5b0bd6d131f7a3a7e524193976ccceb5026607
iraiatorres <iraiatorresrodriguez@gmail.com>
2024-10-22 10:29:51 +0200

    Added external data
    (weather & passengers)

--- avstats/config.py
+++ avstats/config.py
@@ -5,3 +5,10 @@ BASE_DIR = Path(__file__).parent.parent
 
 # Paths to various resources
 DATA_DIR = BASE_DIR / 'data'
+
+# Path to the Excel file
+EXCEL_FILE = DATA_DIR / 'avia_par_be_page_spreadsheet.xlsx'
+
+# Path to the CSV file
+CSV_FILE_SCHED = DATA_DIR / 'avStats_schedule_historical_2023_BRU.csv'
+CSV_FILE_WEATHER = DATA_DIR / 'BRU2023_weather.csv'
