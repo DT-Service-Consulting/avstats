b02c0e822131f6d02e61726e710100cc9f3b4ce5
iraiatorres <iraiatorresrodriguez@gmail.com>
2024-10-22 11:02:09 +0200

    Revert "Added external data"
    This reverts commit 7f5b0bd6d131f7a3a7e524193976ccceb5026607.

--- avstats/imports.py
+++ avstats/imports.py
@@ -8,8 +8,4 @@ import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.ensemble import RandomForestRegressor
 from statsmodels.stats.stattools import durbin_watson
