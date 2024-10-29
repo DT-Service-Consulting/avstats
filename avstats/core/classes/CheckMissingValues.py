7e1604e600ded723e837b00ea923437535c7fa92
Marco <marco@mdigennaro.net>
2024-10-29 12:04:21 +0100

    inserted int64 to check against missing values

--- avstats/core/classes/CheckMissingValues.py
+++ avstats/core/classes/CheckMissingValues.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from typing import Tuple, Any
-
-from pandas import Series, DataFrame
+from typing import Any
 
 
 class DataCleaning:
@@ -26,7 +24,27 @@ class DataCleaning:
             - missing_values (int): The total number of missing values.
             - duplicate_rows (pd.DataFrame): The DataFrame of duplicated rows.
             - missing_by_column (pd.Series): The count of missing values by column.
+
+        Example:
+        >>> data = {
+        ...     'id': [1, 2, 2, 4, 5],
+        ...     'value': [10, 20, 20, None, 50]
+        ... }
+        >>> df = pd.DataFrame(data)
+        >>> cleaner = DataCleaning(unique_column='id')
+        >>> missing_values, duplicate_rows, missing_by_column = cleaner.check_missing_and_duplicates(df)
+        >>> print(missing_values)
+        1
+        >>> print(duplicate_rows)
+           id  value
+        1   2   20.0
+        2   2   20.0
+        >>> print(missing_by_column)
+        id       0
+        value    1
+        dtype: int64
         """
+
         if self.unique_column not in df.columns:
             raise ValueError(f"Column '{self.unique_column}' does not exist in DataFrame")
 
@@ -34,4 +52,5 @@ class DataCleaning:
         duplicate_rows = df[df.duplicated(subset=self.unique_column, keep=False)]
         missing_by_column = df.isnull().sum()
 
+        print(missing_values, missing_values.dtype)
         return missing_values, duplicate_rows, missing_by_column
--- tests/test_CheckMissingValues.py
+++ tests/test_CheckMissingValues.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 import pandas as pd
 from avstats.core.classes.CheckMissingValues import DataCleaning
 
@@ -31,7 +32,16 @@ def test_check_duplicate_rows(clean_data):
     assert len(duplicate_rows) == 2, "Should be 2 duplicated rows"
 
 
+def test_missing_by_column(clean_data):
+    missing_values, duplicate_rows, missing_by_column = clean_data
+    expected_missing_by_column = pd.Series([0, 1], index=['id', 'value'])
+    pd.testing.assert_series_equal(missing_by_column, expected_missing_by_column, check_dtype=False)
+
+
 def test_return_types(clean_data):
     missing_values, duplicate_rows, missing_by_column = clean_data
-    assert isinstance(missing_values, int), "Missing values should be an integer"
+    assert isinstance(missing_values, (int, np.int64)), "Missing values should be an integer or int64"
     assert isinstance(duplicate_rows, pd.DataFrame), "Duplicate rows should be a DataFrame"
+    assert isinstance(missing_by_column, pd.Series), "Missing by column should be a Series"
+
+
